cbce88ff7c22638161ac9c1e6cf153b428d4f237
dorateja293 <dorateja293@users.noreply.github.com>
2025-12-26 10:07:14 +0530

    Initial backend setup

---
+++ server/src/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js"; 
+
+dotenv.config();
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// test route
+app.get("/", (req, res) => {
+  res.send("Backend is running...");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+connectDB();
